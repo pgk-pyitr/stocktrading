301896d8f492db9ed9ac52ecc71bfc644f5fae35
TonyCode <pgktony615@gmail.com>
2023-08-06 17:12:03 -0500

    added to models

--- trading/models.py
+++ trading/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Portfolio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = models.FloatField(default=10000.00)
+
+class Stock(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=10)
+    quantity = models.IntegerField()
