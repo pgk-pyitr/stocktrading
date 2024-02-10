dc44b0cb2256eb93f287cd34ec0617993b3b858e
TonyCode <pgktony615@gmail.com>
2024-02-09 18:10:36 -0600

    added to allowed_hosts

--- stocktrading/settings.py
+++ stocktrading/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zzu!r7o7fv5&a=_rn+z(2wc_d=1rn@+7&6^bx_11f1+87qx#j3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
